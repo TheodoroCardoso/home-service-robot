c784ba0a925866a5f5a55e4a16adce76931936e5
Theodoro Cardoso <cardoso.theodoro@gmail.com>
2021-07-27 18:10:07 +0000

    Add marker package

---
+++ scripts/add_markers.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+xterm  -e  " source devel/setup.bash; roscore" & 
+sleep 5
+xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 1
+xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 1
+xterm  -e  " source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 1
+xterm  -e  " source devel/setup.bash; rosrun add_markers add_markers" &
+
