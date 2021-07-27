538fedf392c6ca1b14feba546e2a9bd6010f99ee
Theodoro Cardoso <cardoso.theodoro@gmail.com>
2021-07-27 13:19:50 +0000

    Test navigation

---
+++ scripts/test_navigation.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+xterm  -e  " source devel/setup.bash; roscore" & 
+sleep 5
+xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 1
+xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 1
+xterm  -e  " source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -2,9 +2,9 @@
 xterm  -e  " source devel/setup.bash; roscore" & 
 sleep 5
 xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo turtlebot_world.launch" &
-sleep 1
+sleep 5
 xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo gmapping_demo.launch" &
-sleep 1
-xterm  -e  " source devel/setup.bash; roslaunch turtlebot_rviz_launchers view_navigation.launch" &
-sleep 1
-xterm  -e  " source devel/setup.bash; roslaunch turtlebot_teleop keyboard_teleop.launch" 
+sleep 5
+xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo view_navigation.launch" &
+sleep 5
+xterm  -e  " source devel/setup.bash; roslaunch turtlebot_gazebo keyboard_teleop.launch" 
